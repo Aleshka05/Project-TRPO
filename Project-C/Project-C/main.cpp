0b19a5fe13a4373e964831e9c5e9d9b89fe68ae0
vlad <vlad.kletsky@yandex.by>
2022-12-22 23:02:15 +0300

    Сделал основную программу

---
+++ Project-C/Project-C/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+
+using namespace std;
+
+void main() {
+
+	int choice = 0;
+	cout << "Выберите программу: \n";
+	cout << "1. Игра угадай число\n";
+	cout << "2. Перевод чисел из 10 системы счисления в другую\n";
+	cout << "3. Простой калькулятор\n";
+	cout << "4. Расписание\n";
+	do {
+		switch (choice)
+		{
+
+		case 1: {
+			game();
+
+			break;
+		}
+		case 2: {
+			system_numser();
+
+			break;
+		}
+		case 3: {
+			calculator();
+
+			break;
+		}
+		case 4: {
+			schedule();
+
+			break;
+		}
+		default:
+			break;
+		}
+	} while (choice != 4);
+}
